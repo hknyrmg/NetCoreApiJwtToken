5d1f0f11cdb43f3090755a04a6b262111aeb9537
hakanyarimaga1202 <hakanyarimaga1202@hotmail.com>
2020-05-03 23:44:37 +0300

    session servis eklendi

---
+++ Utilities/ISessionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TokenBasedAuth_NetCore.Utilities
+{
+    public interface ISessionService
+    {
+        void SetObject(string sessionKey, object sessionObject);
+        T GetObject<T>(string sessionKey);
+
+        void RemoveObject(string sessionKey);
+
+        bool IsNull(string sessionKey);
+
+
+    }
+}
---
+++ Utilities/SessionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TokenBasedAuth_NetCore.Utilities
+{
+    public class SessionService
+    {
+    }
+}
